8614c9231b38eb4432a3356ff87f2efacb917f5b
Pongter Chia <dozeuto@gmail.com>
2025-03-20 15:08:06 +0800

    two-way binding

--- BigMountain/Observable&Subviews/ForecaseView.swift
+++ BigMountain/Observable&Subviews/ForecaseView.swift
@@ -37,6 +37,44 @@ struct WeatherView: View {
     }
 }
 
-#Preview(body: {
+#Preview("One-Way", body: {
     ObservableAndSubview_OneWayView()
+})
+
+struct ObservableAndSubview_TwoWayView: View {
+    @State private var oo = ForecastOO()
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                EditableWeatherView(forecast: oo)
+                
+                Divider()
+                
+                Text(oo.sevenDays.map { $0.day }, format: .list(type: .and, width: .narrow))
+            }
+            .font(.title)
+            .navigationTitle("Weather")
+        }
+    }
+}
+
+struct EditableWeatherView: View {
+    @Bindable var forecast: ForecastOO
+    
+    var body: some View {
+        List($forecast.sevenDays) { $day in
+            Label {
+                TextField("Day", text: $day.day)
+                    .textFieldStyle(.roundedBorder)
+            } icon: {
+                Image(systemName: day.icon)
+            }
+        }
+        .tint(.pink)
+    }
+}
+
+#Preview("Two-Way", body: {
+    ObservableAndSubview_TwoWayView()
 })
