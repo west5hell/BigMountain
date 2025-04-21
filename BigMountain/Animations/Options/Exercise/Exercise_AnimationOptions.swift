bf004d4d9f0cd0c1c18e0267aa5bd71ea10446cf
Ponter Chia <dozeuto@gmail.com>
2025-04-21 13:03:00 +0800

    add Exercise_Animation_TheLayout view and color assets for enhanced UI design

---
+++ BigMountain/Animations/Options/Exercise/Exercise_AnimationOptions.swift
@@ -0,0 +1,57 @@
+//
+//  Exercise_AnimationOptions.swift
+//  BigMountain
+//
+//  Created by Pongt Chia on 21/4/25.
+//
+
+import SwiftUI
+
+struct Exercise_AnimationOptions: View {
+    @State private var userName = ""
+    @State private var password = ""
+    
+    var body: some View {
+        ZStack {
+            RadialGradient(
+                gradient: Gradient(colors: [Color("Dark"), Color("Darkest")]),
+                center: .center,
+                startRadius: 10,
+                endRadius: 400
+            )
+            .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                Text("Exercise")
+                    .foregroundStyle(Color("Gold"))
+                    .font(.system(size: 70).bold())
+                Text("Show Login")
+                    .foregroundStyle(.gray)
+                Spacer()
+                VStack(spacing: 30) {
+                    Image(systemName: "lock.shield")
+                        .font(.system(size: 70))
+                        .padding()
+                        .foregroundStyle(Color("Gold"))
+                    TextField("Username", text: $userName)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    SecureField("Password", text: $password)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                    Button("Login") {
+                        
+                    }
+                    .foregroundStyle(Color("Accent"))
+                }
+                .padding(.horizontal)
+                
+                Spacer()
+                Spacer()
+            }
+        }
+        .font(.title)
+    }
+}
+
+#Preview {
+    Exercise_AnimationOptions()
+}
