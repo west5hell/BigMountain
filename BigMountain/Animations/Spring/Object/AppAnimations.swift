1f0fd60e6d17948d159308baa7f8da35b7fa7408
Ponter Chia <dozeuto@gmail.com>
2025-04-21 21:25:19 +0800

    add SpringObject_Presets_Customization view for interactive spring animation demonstration

---
+++ BigMountain/Animations/Spring/Object/AppAnimations.swift
@@ -0,0 +1,14 @@
+//
+//  AppAnimations.swift
+//  BigMountain
+//
+//  Created by Pongt Chia on 21/4/25.
+//
+
+import SwiftUI
+
+class AppAnimations {
+    static var tightSpring = Spring(response: 0.2, dampingRatio: 0.6)
+    static var heavySpring = Spring(mass: 1.0, stiffness: 8, damping: 0.4)
+    static var looseSpring = Spring(settlingDuration: 0.8, dampingRatio: 0.6)
+}
