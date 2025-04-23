ada2e2074466897f93dfa1d4a35faeacce3f6208
Ponter Chia <dozeuto@gmail.com>
2025-04-23 14:58:41 +0800

    add ShiftingViews_Problem view to demonstrate transitions with view insertion and removal

---
+++ BigMountain/Animations/Transitions/Transitions and Space/ShiftingViews_Problem.swift
@@ -0,0 +1,47 @@
+//
+//  ShiftingViews_Problem.swift
+//  BigMountain
+//
+//  Created by Pongt Chia on 23/4/25.
+//
+
+import SwiftUI
+
+struct ShiftingViews_Problem: View {
+    @State private var change = false
+    
+    var body: some View {
+        ZStack {
+            Color("DarkShade2")
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                HeaderView(
+                    "Transitions",
+                    subtitle: "Shifting Views Problem",
+                    desc: "Transitions work with views that are inserted and removed from the screen (view hierarchy). Insertion and removal will adjust views around them.",
+                    back: Color("DarkAccent2"),
+                    textColor: Color("LightShade2")
+                )
+                
+                Button("Change") {
+                    change.toggle()
+                }
+                
+                if change {
+                    Image("Phone")
+                        .transition(.slide)
+                }
+                
+                DescView("Notice what happens when the view is inserted here.")
+            }
+        }
+        .animation(.default, value: change)
+        .font(.title)
+        .tint(Color("Accent2"))
+    }
+}
+
+#Preview {
+    ShiftingViews_Problem()
+}
