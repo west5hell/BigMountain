c3753517707927366f695ebe93fda621c51f47d3
Ponter Chia <dozeuto@gmail.com>
2025-04-23 18:55:00 +0800

    add: create Curve_Exercise view with initial SwiftUI setup

---
+++ BigMountain/Animations/Custom Timing Curve/Exercise/Curve_Exercise.swift
@@ -0,0 +1,18 @@
+//
+//  Curve_Exercise.swift
+//  BigMountain
+//
+//  Created by Pongt Chia on 23/4/25.
+//
+
+import SwiftUI
+
+struct Curve_Exercise: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Curve_Exercise()
+}
