6310997961443fe24fc2ff3c942866f6baa4f121
Ponter Chia <dozeuto@gmail.com>
2025-04-23 18:36:34 +0800

    add: implement UnitCurve_Intro view with custom timing curve animations

--- BigMountain/Animations/Spring/Object/AppAnimations.swift
+++ BigMountain/Animations/Spring/Object/AppAnimations.swift
@@ -11,4 +11,9 @@ class AppAnimations {
     static var tightSpring = Spring(response: 0.2, dampingRatio: 0.6)
     static var heavySpring = Spring(mass: 1.0, stiffness: 8, damping: 0.4)
     static var looseSpring = Spring(settlingDuration: 0.8, dampingRatio: 0.6)
+    
+    static var customCurve = UnitCurve.bezier(
+        startControlPoint: UnitPoint(x: 0.5, y: -0.5),
+        endControlPoint: UnitPoint(x: 0.5, y: 1.5)
+    )
 }
