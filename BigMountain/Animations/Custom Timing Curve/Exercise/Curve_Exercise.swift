4482fdf57b32114cc03b2f1a8534683073faee3a
Ponter Chia <dozeuto@gmail.com>
2025-04-23 19:48:31 +0800

    add: enhance Curve_Exercise view with user greeting, image, and details button

--- BigMountain/Animations/Custom Timing Curve/Exercise/Curve_Exercise.swift
+++ BigMountain/Animations/Custom Timing Curve/Exercise/Curve_Exercise.swift
@@ -25,6 +25,47 @@ struct Curve_Exercise: View {
                     .padding(.top, 40)
                 Spacer()
             }
+            
+            VStack(spacing: 20) {
+                Text("Welcome Back,")
+                    .fontWeight(.thin)
+                    .padding(.top)
+                Text("Jaqueline")
+                    .fontWeight(.bold)
+                    .font(.largeTitle)
+                Image("JOJO-Liu")
+                    .resizable()
+                    .frame(width: 200, height: 200)
+                    .clipShape(Circle())
+                
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Text("See Details")
+                        .fontWeight(.light)
+                        .frame(maxWidth: .infinity)
+                }
+                .padding()
+                .foregroundStyle(.white)
+                .background(
+                    RoundedRectangle(cornerRadius: 15)
+                        .fill(Color("Accent4"))
+                        .opacity(0.1)
+                )
+                .overlay {
+                    RoundedRectangle(cornerRadius: 15)
+                        .stroke(Color("Accent4"), lineWidth: 0.7)
+                }
+                .padding()
+            }
+            .frame(width: 300, height: 600)
+            .background(
+                RoundedRectangle(cornerRadius: 20)
+                    .fill(Color("Secondary4"))
+                    .shadow(radius: 8)
+            )
         }
         .font(.title)
         .foregroundStyle(Color("Foreground4"))
