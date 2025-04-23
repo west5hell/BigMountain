1076261a7d3b725a67cc42775917e54b7e61089f
Ponter Chia <dozeuto@gmail.com>
2025-04-23 15:15:38 +0800

    add: implement Asymmetric_TwoAnimations view to showcase asymmetric transitions

---
+++ BigMountain/Animations/Transitions/Asymmetric Transitions/Asymmetric_TwoAnimations.swift
@@ -0,0 +1,67 @@
+//
+//  Asymmetric_TwoAnimations.swift
+//  BigMountain
+//
+//  Created by Pongt Chia on 23/4/25.
+//
+
+import SwiftUI
+import MapKit
+
+struct Asymmetric_TwoAnimations: View {
+    @State private var showHelp = false
+    
+    var body: some View {
+        ZStack {
+            Color("DarkShade2")
+                .ignoresSafeArea()
+            
+            VStack(spacing: 20) {
+                HeaderView(
+                    "Transitions",
+                    subtitle: "Asymmetric Introduction",
+                    desc: "Provide a different transition for the insertion and removal of a view."
+                )
+                
+                ZStack(alignment: .topTrailing) {
+                    Map()
+                    Button {
+                        withAnimation(.spring()) {
+                            showHelp = true
+                        }
+                    } label: {
+                        Image(systemName: "questionmark.circle.fill")
+                    }.padding()
+                }.ignoresSafeArea(edges: .vertical)
+            }
+            
+            if showHelp {
+                VStack(spacing: 20) {
+                    Text("Help")
+                        .foregroundStyle(Color("LightShade2"))
+                    Image("Phone")
+                    Button("Close") {
+                        withAnimation(.easeIn) {
+                            showHelp = false
+                        }
+                    }.padding(.top, 30)
+                }
+                .padding(35)
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .fill(Color("DarkAccent2"))
+                )
+                .padding(.top, 100)
+                .shadow(radius: 15)
+                .transition(.asymmetric(insertion: .slide, removal: .move(edge: .bottom)))
+                .zIndex(1)
+            }
+        }
+        .tint(Color("Accent2"))
+        .font(.title)
+    }
+}
+
+#Preview {
+    Asymmetric_TwoAnimations()
+}
