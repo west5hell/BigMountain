5e1998b05b4b51b575413c2db8cf5f31583814bc
Ponter Chia <dozeuto@gmail.com>
2025-04-24 23:49:47 +0800

    add: implement MGE_WhatCouldGoWrong_FrameProblem view to demonstrate matched geometry effect frame issues

---
+++ BigMountain/Animations/Matched Geometry Effect/Problems/MGE_WhatCouldGoWrong_FrameProblem.swift
@@ -0,0 +1,57 @@
+//
+//  MGE_WhatCouldGoWrong_FrameProblem.swift
+//  BigMountain
+//
+//  Created by Pongt Chia on 24/4/25.
+//
+
+import SwiftUI
+
+struct MGE_WhatCouldGoWrong_FrameProblem: View {
+    @Namespace var namespace
+    
+    @State private var showView2 = false
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            HeaderView(
+                "MatchedGeometryEffect",
+                subtitle: "Frame Problem",
+                desc: "The order of modifiers matters. View 1 is now animating in an unexpected way. Can you spot the problem here?",
+                back: .green,
+                textColor: .black
+            )
+            
+            Spacer()
+            
+            if showView2 {
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(.green)
+                    .overlay(content: {
+                        Text("View 2")
+                    })
+                    .matchedGeometryEffect(id: "change", in: namespace)
+                    .onTapGesture {
+                        showView2.toggle()
+                    }
+            } else {
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(.green)
+                    .overlay(content: {
+                        Text("View 1")
+                    })
+                    .frame(width: 160, height: 160)
+                    .matchedGeometryEffect(id: "change", in: namespace)
+                    .onTapGesture {
+                        showView2.toggle()
+                    }
+            }
+        }
+        .animation(.default, value: showView2)
+        .font(.title)
+    }
+}
+
+#Preview {
+    MGE_WhatCouldGoWrong_FrameProblem()
+}
