381ed7b86c24befc5fb91efc009806ae6616d13b
Ponter Chia <dozeuto@gmail.com>
2025-04-24 23:43:25 +0800

    add: implement MGE_OneSource view to demonstrate matched geometry effect with a single source view

---
+++ BigMountain/Animations/Matched Geometry Effect/Animation/MGE_OneSource.swift
@@ -0,0 +1,59 @@
+//
+//  MGE_OneSource.swift
+//  BigMountain
+//
+//  Created by Pongt Chia on 24/4/25.
+//
+
+import SwiftUI
+
+struct MGE_OneSource: View {
+    @Namespace var namespace
+    
+    @State private var change = false
+    
+    var body: some View {
+        VStack(spacing: 20) {
+            HeaderView(
+                "MatchedGeometryEffect",
+                subtitle: "When One View is Source",
+                desc: "You can set one view as source. The animation will look good when the source gets hidden. Not so good when shown.",
+                back: .green,
+                textColor: .black
+            )
+            
+            Text("Blue is the source")
+            
+            HStack {
+                if change == false {
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(.blue)
+                        .matchedGeometryEffect(id: "id", in: namespace)
+                        .frame(width: 60, height: 60)
+                        .onTapGesture {
+                            change.toggle()
+                        }
+                }
+                
+                Spacer()
+                
+                if change {
+                    RoundedRectangle(cornerRadius: 16)
+                        .fill(.green)
+                        .matchedGeometryEffect(id: "id", in: namespace, isSource: false)
+                        .frame(width: 160, height: 160)
+                        .onTapGesture {
+                            change.toggle()
+                        }
+                }
+            }
+            .frame(height: 160)
+        }
+        .animation(.easeIn, value: change)
+        .font(.title)
+    }
+}
+
+#Preview {
+    MGE_OneSource()
+}
