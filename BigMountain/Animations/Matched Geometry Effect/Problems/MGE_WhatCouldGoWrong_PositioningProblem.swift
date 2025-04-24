4477112282ca9ace2e0332d5647762ad59b981ab
Ponter Chia <dozeuto@gmail.com>
2025-04-24 23:55:49 +0800

    add: implement MGE_WhatCouldGoWrong_PositioningProblem view to illustrate positioning issues with matched geometry effect

--- BigMountain/Animations/Matched Geometry Effect/Problems/MGE_WhatCouldGoWrong_PositioningProblem.swift
+++ BigMountain/Animations/Matched Geometry Effect/Problems/MGE_WhatCouldGoWrong_PositioningProblem.swift
@@ -8,8 +8,48 @@
 import SwiftUI
 
 struct MGE_WhatCouldGoWrong_PositioningProblem: View {
+    @Namespace var namespace
+    
+    @State private var showView2 = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 20) {
+            HeaderView(
+                "MatchedGeometryEffect",
+                subtitle: "Positioning Problem",
+                desc: "Here's an example of how a positioning modifier can also create unexpected results.",
+                back: .green,
+                textColor: .black
+            )
+            
+            Spacer()
+            
+            if showView2 {
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(.green)
+                    .overlay(content: {
+                        Text("View 2")
+                    })
+                    .matchedGeometryEffect(id: "change", in: namespace)
+                    .onTapGesture {
+                        showView2.toggle()
+                    }
+            } else {
+                RoundedRectangle(cornerRadius: 25)
+                    .fill(.green)
+                    .overlay(content: {
+                        Text("View 1")
+                    })
+                    .offset(x: -130)
+                    .matchedGeometryEffect(id: "change", in: namespace)
+                    .frame(width: 100, height: 100)
+                    .onTapGesture {
+                        showView2.toggle()
+                    }
+            }
+        }
+        .animation(.default, value: showView2)
+        .font(.title)
     }
 }
 
