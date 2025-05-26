25c0e74254b2bfaedad8401c675bbdfe6820eb46
Ponter Chia <dozeuto@gmail.com>
2025-05-26 15:18:28 +0800

    update BigMountainApp.swift file

--- BigMountain/BigMountainApp.swift
+++ BigMountain/BigMountainApp.swift
@@ -10,22 +10,10 @@ import SwiftData
 
 @main
 struct BigMountainApp: App {
-    let container: ModelContainer
     
-    init() {
-        #if DEBUG
-        self.container = PlanetModel.preview
-        #else
-        self.container = try! ModelContainer(for: PlanetModel.self)
-        #endif
-    }
     var body: some Scene {
         WindowGroup {
-            OO_InjectionExample(modelContext: container.mainContext)
-                .modelContainer(container)
-//            Theoretically, you shouldn’t even need the
-//            modelContainer modifier if you are not using @Query or
-//            @Environment(\.modelContext).
+            ContentView()
         }
     }
 }
