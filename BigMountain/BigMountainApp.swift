deb507e2c80276396db8ef50c432640e0f30bb2f
Ponter Chia <dozeuto@gmail.com>
2025-05-26 12:47:22 +0800

    Refactor BigMountainApp to use OO_InjectionExample for model context management

--- BigMountain/BigMountainApp.swift
+++ BigMountain/BigMountainApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct BigMountainApp: App {
+    let container: ModelContainer
+    
+    init() {
+        #if DEBUG
+        self.container = PlanetModel.preview
+        #else
+        self.container = try! ModelContainer(for: PlanetModel.self)
+        #endif
+    }
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            OO_InjectionExample(modelContext: container.mainContext)
+                .modelContainer(container)
+//            Theoretically, you shouldn’t even need the
+//            modelContainer modifier if you are not using @Query or
+//            @Environment(\.modelContext).
         }
     }
 }
