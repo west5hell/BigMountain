b3ef7ef69423ec4d71abec5bba470d941fd74f90
Ponter Chia <dozeuto@gmail.com>
2025-05-27 20:58:36 +0800

    Add URLSessionView for displaying and importing friends data

---
+++ BigMountain/URLSession/URLSessionView.swift
@@ -0,0 +1,50 @@
+//
+//  URLSessionView.swift
+//  BigMountain
+//
+//  Created by Pongt Chia on 27/5/25.
+//
+
+import SwiftData
+import SwiftUI
+
+struct URLSessionView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var friends: [FriendModel]
+
+    var body: some View {
+        NavigationStack {
+            List {
+                Section("Total: \(friends.count)") {
+                    ForEach(friends) { friend in
+                        Text(friend.viewName, format: .name(style: .medium))
+                            .font(.title)
+                    }
+                }
+            }
+            .headerProminence(.increased)
+            .navigationTitle("People")
+            .toolbar {
+                Button("", systemImage: "person.crop.circle.fill.badge.plus") {
+                    let container = modelContext.container
+
+                    Task.detached {
+                        let bgActor = FriendModel.BackgroundActor(
+                            modelContainer: container
+                        )
+
+                        do {
+                            try await bgActor.importFriends()
+                        } catch {
+                            print("Error: \(error.localizedDescription)")
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    URLSessionView()
+}
