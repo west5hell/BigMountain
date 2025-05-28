e8801cafe20e19959695a835907622b80ca0676e
Ponter Chia <dozeuto@gmail.com>
2025-05-28 14:15:12 +0800

    Refactor MountainModel to introduce versioned schemas and add elevation property in version 2

--- BigMountain/Versioning/MountainModel.swift
+++ BigMountain/Versioning/MountainModel.swift
@@ -8,18 +8,42 @@
 import SwiftData
 import SwiftUI
 
-@Model
-class MountainModel {
-    var name: String
-    var country: String
-    var image: Data? = nil
+//@Model
+//class MountainModel {
+//    var name: String
+//    var country: String
+//    var image: Data? = nil
+//
+//    init(name: String, country: String, image: Data? = nil) {
+//        self.name = name
+//        self.country = country
+//        self.image = image
+//    }
+//}
 
-    init(name: String, country: String, image: Data? = nil) {
-        self.name = name
-        self.country = country
-        self.image = image
+enum MountainModelSchemaV1: VersionedSchema {
+
+    static var versionIdentifier: Schema.Version = Schema.Version(1, 0, 0)
+
+    static var models: [any PersistentModel.Type] {
+        [MountainModel.self]
+    }
+
+    @Model
+    class MountainModel {
+        var name: String
+        var country: String
+        var image: Data? = nil
+
+        init(name: String, country: String, image: Data? = nil) {
+            self.name = name
+            self.country = country
+            self.image = image
+        }
     }
 }
+
+typealias MountainModel = MountainModelSchemaV1.MountainModel
 
 extension MountainModel {
     var viewImage: UIImage {
@@ -27,6 +51,30 @@ extension MountainModel {
             return image
         } else {
             return UIImage(resource: .earth)
+        }
+    }
+}
+
+enum MountainModelSchemaV2: VersionedSchema {
+    static var versionIdentifier = Schema.Version(2, 0, 0)
+
+    static var models: [any PersistentModel.Type] {
+        [MountainModel.self]
+    }
+
+    @Model
+    class MountainModel {
+        var name: String
+        var country: String
+        var elevation: Int = 0
+        var image: Data? = nil
+
+        init(name: String, country: String, elevation: Int, image: Data? = nil)
+        {
+            self.name = name
+            self.country = country
+            self.elevation = elevation
+            self.image = image
         }
     }
 }
