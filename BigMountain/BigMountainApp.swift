a439a809ef1617b0b28d0a552eb784cefce99959
Ponter Chia <dozeuto@gmail.com>
2025-05-28 14:23:41 +0800

    Refactor BigMountainApp to use ModelContainer for MountainModel with migration plan

--- BigMountain/BigMountainApp.swift
+++ BigMountain/BigMountainApp.swift
@@ -5,16 +5,31 @@
 //  Created by Pongt Chia on 17/3/25.
 //
 
-import SwiftUI
 import SwiftData
+import SwiftUI
 
 @main
 struct BigMountainApp: App {
-    
+
     var body: some Scene {
         WindowGroup {
             URLSessionView()
-                .modelContainer(for: FriendModel.self)
+                .modelContainer(container)
+        }
+    }
+
+    @MainActor
+    var container: ModelContainer {
+        do {
+            let container = try ModelContainer(
+                for: MountainModel.self,
+                migrationPlan: MountainModelMigrationPlan.self
+            )
+            return container
+        } catch {
+            fatalError(
+                "Could not create ModelContainer with migration plan: \(error.localizedDescription)"
+            )
         }
     }
 }
