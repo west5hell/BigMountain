6d9ec59abc0045b08a1d5783c223219373c038f2
Ponter Chia <dozeuto@gmail.com>
2025-05-30 16:57:04 +0800

    Add initial Xcode scheme and remove unused book data structures

--- BigMountain/Templates/DescView.swift
+++ BigMountain/Templates/DescView.swift
@@ -9,25 +9,19 @@ import SwiftUI
 
 struct DescView: View {
     var desc = "Use this to…"
-    var back = Color.orange
-    var textColor = Color.black
     
     init(
         _ desc: String = "Use this to…",
-        back: Color = Color.orange,
-        textColor: Color = Color.black
     ) {
         self.desc = desc
-        self.back = back
-        self.textColor = textColor
     }
     
     var body: some View {
         Text(desc)
             .frame(maxWidth: .infinity)
             .padding()
-            .background(back)
-            .foregroundStyle(textColor)
+            .background(Color.purple)
+            .foregroundStyle(.white)
     }
 }
 
